1360c6cb970263996a7ccdd2a65878688528c5a6
Henrique Barros <henriquesbarroshs@gmail.com>
2021-11-17 23:06:27 -0300

    feat: creation of header component html

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <figure>
+          <img src="Logo.svg" alt="Logo" />
+        </figure>
+      </div>
+    </header>
+  );
 }
