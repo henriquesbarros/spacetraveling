71c25167f3c127cfaad60a6cbd4109de45ac07d2
Henrique Barros <henriquesbarroshs@gmail.com>
2021-11-22 08:29:45 -0300

    feat: insertion of link to return to home page in header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
+import Link from 'next/link';
+
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.container}>
       <div className={styles.content}>
-        <figure>
-          <img src="Logo.svg" alt="Logo" />
-        </figure>
+        <Link href="/">
+          <a>
+            <figure>
+              <img src="/Logo.svg" alt="logo" />
+            </figure>
+          </a>
+        </Link>
       </div>
     </header>
   );
